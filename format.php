8d83e930e7c57080eb9e30f4a22d320d384fc46c
unknown <Administrador@ORD-25-9.CUATROVIENTOS.local>
2015-10-19 20:31:04 +0200

    First commit

---
+++ format.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Text format</title>
+	</head>
+	<body>
+		<p>
+		This is <b>important</b>
+		</p>
+		<p>
+		Solo somos <i>amigos</i>
+		</p>
+	</body>
+</html>
